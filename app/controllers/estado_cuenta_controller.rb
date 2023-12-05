debf47d9d8ef4c9304e94fc914e1c833565545f6
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-12-05 16:27:11 -0500

    correccion

--- app/controllers/estado_cuenta_controller.rb
+++ app/controllers/estado_cuenta_controller.rb
@@ -1,4 +1,4 @@
-class EstadosCuentaController < ApplicationController
+class EstadoCuentaController < ApplicationController
     def buscar_estado
       @estado_cuenta = EstadoCuenta.where(paciente: params[:paciente])
   
