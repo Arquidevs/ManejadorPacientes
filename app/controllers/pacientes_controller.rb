8a5ff2c3e5543796f8e776cc5f9ffd69eebd4f26
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-12-05 01:04:12 -0500

    Update pacientes_controller.rb

--- app/controllers/pacientes_controller.rb
+++ app/controllers/pacientes_controller.rb
@@ -1,24 +1,17 @@
-  # app/controllers/pacientes_controller.rb
-  class PacientesController < ApplicationController
-    # Otras acciones del controlador...
-    def index
-      @pacientes = Paciente.all
-    end
-  
-    def buscar_paciente
-      @paciente = Paciente.find_by(id: params[:id])
-      respond_to do |format|
-        format.html do
-          # Lógica para manejar solicitudes HTML
-        end
-        format.json do
-          if @paciente
-            render json: @paciente
-          else
-            render json: { error: 'Paciente no encontrado' }, status: :not_found
+# app/controllers/pacientes_controller.rb
 
-          end
-        end
-      end
+class PacientesController < ApplicationController
+  def index
+    @pacientes = Paciente.all
+  end
+
+  def buscar_paciente
+    @paciente = Paciente.find_by(cedula: params[:cedula])
+
+    if @paciente
+      render json: @paciente
+    else
+      render json: { error: 'Paciente no encontrado' }, status: :not_found
     end
   end
+end
