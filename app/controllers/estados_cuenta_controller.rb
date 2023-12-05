4569e2f4fec90e02261981d7dd861b75787a8fb2
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-12-05 01:36:47 -0500

    Update estados_cuenta_controller.rb

--- app/controllers/estados_cuenta_controller.rb
+++ app/controllers/estados_cuenta_controller.rb
@@ -1,13 +1,13 @@
 class EstadosCuentaController < ApplicationController
 
     def buscar_estado
-      @estados_cuenta = EstadoCuenta.where(paciente: params[:paciente])
+      @estado_cuenta = EstadoCuenta.where(paciente: params[:paciente])
   
       respond_to do |format|
         format.html { head :no_content }
         format.json do
-          if @estados_cuenta.any?
-            render json: @estados_cuenta
+          if @estado_cuenta.any?
+            render json: @estado_cuenta
           else
             render json: { error: 'Estados de cuenta no encontrados' }, status: :not_found
           end
